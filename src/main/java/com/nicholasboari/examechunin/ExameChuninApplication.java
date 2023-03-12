5e99fecc9f82c5296d2915199af701ab701e1d75
nicholasboari <nicholasboari@gmail.com>
2023-03-12 01:02:01 -0300

    project created

---
+++ src/main/java/com/nicholasboari/examechunin/ExameChuninApplication.java
@@ -0,0 +1,13 @@
+package com.nicholasboari.examechunin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameChuninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExameChuninApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicholasboari/examechunin/ExameChuninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicholasboari.examechunin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExameChuninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
